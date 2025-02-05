d83f520bbe438d2988f21faae2c0b7bd1fd109d5
lucasadilla <lucas.pentlandhyde@gmail.com>
2025-02-04 23:13:57 -0500

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,12 @@ import '../styles/global.css'; // Import the global CSS file
 import { DefaultSeo } from 'next-seo';
 import SEO from '../next-seo.config';
 import Script from 'next/script';
+import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
+            <Analytics />
             <Script
                 src="https://www.youtube.com/iframe_api"
                 strategy="lazyOnload"
